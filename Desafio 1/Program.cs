4626ebb2397f1737e23687c684be0f8b73e9253b
Monara Almeida <monaraalmeida23@outlook.com>
2023-06-13 11:20:07 -0300

    Adicionar arquivos de projeto.

---
+++ Desafio 1/Program.cs
@@ -0,0 +1,45 @@
+﻿//escolha um numero entre 1 e 100.
+
+int numeroSorteado = 45;
+    void advinharNumero()
+    {
+    Console.WriteLine("Escolha um número entre 1 e 100.");
+    Console.Write("\nAgora digite-o: ");
+    }
+
+    advinharNumero();
+
+do
+{
+    string opcaoEscolhida = Console.ReadLine()!;
+    int opcaoEscolhidaNumero = int.Parse(opcaoEscolhida);
+
+
+
+    if (opcaoEscolhidaNumero == numeroSorteado)
+    {
+        Console.WriteLine("Você acertou PARABÉNSSS!");
+        break;
+    }
+    else if (opcaoEscolhidaNumero > 100)
+    {
+        Console.WriteLine("O número escolhido é maior do numero sorteado.\nDigite outro palpite: ");
+    }
+    else
+    {
+        Console.WriteLine("O número escolhido é menor do numero sorteado.\nDigite outro palpite: ");
+    }
+}
+while (true);
+
+   
+
+
+
+
+
+
+
+
+
+
